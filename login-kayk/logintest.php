3afd812adad0b2eeb4147f8b56695cacee2531c7
KayckRossi <Kayck.12gabriel@gmail.com>
2022-11-28 22:10:04 -0300

    Subidno pasta Kayk

---
+++ login-kayk/logintest.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="teststyle.css">
+	<title>Login</title>
+</head>
+<body>
+	<div class="main-login">
+
+		<div class="pt-esquerda-login">
+
+
+			<img src="c.png" class="left-login-img" alt="cadeado">
+
+		</div>
+
+
+		<div class="pt-direita-login">
+
+			<div class="card-login">
+
+				<h1>Login</h1>
+
+				<div class="form">
+					
+					<label for="RM">RM:</label>
+					<input type="number" name="RM" placeholder="RM">
+
+				</div>
+				<div class="form">
+
+					<label for="senha">Senha:</label>
+					<input type="password" name="senha" placeholder="Senha">
+
+				</div>
+
+				<button class="bnt-login"><a href="#">Entrar</a></button>
+				<button class="bnt-login"><a href="#">Cadastre-se</a></button>
+
+				<a href="#">esqueci minha senha</as>
+			</div>
+		</div>
+
+		
+
+
+	</div><!--main-login-->
+
+</body>
+</html>
