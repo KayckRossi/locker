28a0d8c03abb8d84c35f1789c73fb89ad6e770c2
KayckRossi <Kayck.12gabriel@gmail.com>
2022-12-07 15:22:43 -0300

    subindo pagina Login, ver depois a responsividade

---
+++ view/dashboard/loginofc.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../public/assets/plugins/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../public/assets/plugins/fontawesome-free-6.2.0-web/css/all.min.css">
+    <title>LockEtec</title>
+</head>
+
+<body>
+
+
+    <div class="container">
+
+        <div class="row">
+            <div class="login" style="padding-top: 5rem;  padding-left: 40rem;">
+                <div class="card" style="width: 30rem; height:30rem;  border-radius: 20px 20px 20px 20px;">
+                    <div class="card-body">
+                        <h1 style="margin-left:11rem;">Login</h1>
+                        <form action="">
+
+                            <label for="rm" style="margin-left:1rem ;">RM:</label>
+                            <input type="number" class="form-control" style="margin-left: 1rem; width: 25rem; height: 3rem; border-radius: 15px 15px 15px 15px;" id="rm" name="rm" placeholder="RM" required>
+
+
+                            <label for="senha" style="margin-left:1rem ;">Senha:</label>
+                            <input type="password" class="form-control" style="margin-left: 1rem; width: 25rem; height: 3rem; border-radius: 15px 15px 15px 15px;" id="rm" name="rm" placeholder="Senha" required>
+
+
+                            <div class="btns" style=" padding:1rem;">
+
+                                <button type="button" class="btn btn-warning" style="padding:16px; width:100%; border-radius:15px 15px 15px 15px;">ENTRAR</button>
+
+                            </div>
+
+                            <div class="btns" style="padding:1rem;">
+
+                                <button type="button" class="btn btn-warning" style="padding:16px;width:100%; border-radius:15px 15px 15px 15px;">CADASTRA-SE</button>
+
+                            </div>
+
+
+                        </form>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+    <script src="../../public/assets/plugins/fontawesome-free-6.2.0-web/js/all.min.js"></script>
+</body>
+
+</html>
